f8c6bb535e2a21277dbc623c8468f1e94e73bd9c
Vladimir Jimenez <allejo@me.com>
2020-12-05 16:05:27 -0800

    Format main.ts with prettier

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import * as core from '@actions/core'
-import { sendIRCMessage } from './irc/messaging'
-import { applyControl, ControlCode } from './irc/formatting'
+import {sendIRCMessage} from './irc/messaging'
+import {applyControl, ControlCode} from './irc/formatting'
 
 async function run(): Promise<void> {
   try {
-    await sendIRCMessage(`Just a simple test from ${applyControl('allejo', ControlCode.Bold)}`, '+sujevo-dev');
-
+    await sendIRCMessage(
+      `Just a simple test from ${applyControl('allejo', ControlCode.Bold)}`,
+      '+sujevo-dev'
+    )
   } catch (error) {
     core.setFailed(error.message)
   }

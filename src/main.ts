48dac76d7a52aa8a70ba8edb0faaad404d4197e6
Vladimir Jimenez <allejo@me.com>
2020-12-07 00:34:21 -0800

    Get channels & messages from GH Actions inputs

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import * as core from '@actions/core'
 import {sendIRCMessage} from './irc/messaging'
-import {applyControl, ControlCode} from './irc/formatting'
+import {formatStr} from './irc/markup_parser'
 
 async function run(): Promise<void> {
   try {
     await sendIRCMessage(
-      `Just a simple test from ${applyControl('allejo', ControlCode.Bold)}`,
-      '+sujevo-dev'
+      formatStr(core.getInput('message', {required: true})),
+      core.getInput('channel', {required: true})
     )
   } catch (error) {
     core.setFailed(error.message)
